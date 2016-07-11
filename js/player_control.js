e597c1641775b52762cfc601e783d3d2cd9a1d93
Murat Hasdemir <murathasdemir0@gmail.com>
2016-07-11 21:22:30 +0300

    11.07.2016

--- js/player_control.js
+++ js/player_control.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
-var video = document.getElementById('video');
-var playpause = document.getElementById('play_pause');
-var sound = document.getElementById('sound');
-var progress = document.getElementById("progress");
-var volumeBar = document.getElementById("volume");
-var full_screen = document.getElementById('full_screen');
-var currentTimeDisplay = document.getElementById('current');
-var totalVideoTime = document.getElementById('lenght');
+var video               = document.getElementById('video');
+var playpause           = document.getElementById('play_pause');
+var sound               = document.getElementById('sound');
+var progress            = document.getElementById("progress");
+var volumeBar           = document.getElementById("volume");
+var full_screen         = document.getElementById('full_screen');
+var currentTimeDisplay  = document.getElementById('current');
+var totalVideoTime      = document.getElementById('lenght');
+var captions            = document.getElementById('captionArea');
+var captionButton       = document.getElementById('cc');
+var captionTextArea     = document.getElementById('captionTextArea');
+var player_container    = document.getElementById('player_container');
 
 function playPause(){
 
@@ -39,15 +43,23 @@ $('.video_container').hover(function(){
         $('#player_container').css("bottom", "72px");
         }, function(){
         $('#controls').css("display", "none");
-        $('#player_container').css("bottom", "30px");
+        $('#player_container').css("bottom", "42px");
     });
 full_screen.addEventListener("click", function() {
   if (video.requestFullscreen) {
     video.requestFullscreen();
+        player_container.classList.add('absolute'); 
+        player_container.classList.add('fullscreen'); 
   } else if (video.mozRequestFullScreen) {
-    video.mozRequestFullScreen(); // Firefox
+    video.mozRequestFullScreen();
+        player_container.classList.add('absolute');
+        player_container.classList.add('fullscreen');  // Firefox 
   } else if (video.webkitRequestFullscreen) {
-    video.webkitRequestFullscreen(); // Chrome and Safari
+    video.webkitRequestFullscreen();
+        player_container.classList.add('absolute');
+        player_container.classList.add('fullscreen');  // Chrome and Safari
+  }else if (video.fullscreenElement && video.fullscreenElement.nodeName == 'video') {
+    console.log('Your video is playing in fullscreen');
   }
 });
 
@@ -78,46 +90,74 @@ function highlight(num) {
 }
 function time(){
   var curmins = Math.floor(video.currentTime / 60);
-  var cursecs = Math.floor(video.currentTime - curmins * 60);
+  if (curmins < 10) {
+    curmins = '0'+curmins;
+  };
+  var cursecs = Math.floor(video.currentTime - curmins * 60); 
+  if (cursecs < 10) {
+    cursecs = '0'+cursecs;
+  };
   var durmins = Math.floor(video.duration / 60);
+  if (durmins < 10) {
+    durmins = '0'+durmins;
+  };
   var dursecs = Math.floor(video.duration - durmins * 60);
+  if (dursecs < 10) {
+    dursecs = '0'+dursecs;
+  };
   totalVideoTime.innerHTML = durmins+":"+dursecs;
   currentTimeDisplay.innerHTML = curmins+":"+cursecs;
 
 
   var time = video.currentTime.toFixed(2); 
    if (time >= 0.18 && time < 4.13) {
-            highlight('#1'); 
+            highlight('#1');  
+            captionTextArea.innerHTML = document.getElementById("1").innerText;   
     } else if (time >= 4.13 && time < 7.54) {
             highlight('#2');
+            captionTextArea.innerHTML = document.getElementById("2").innerText; 
     } else if (time >= 7.54 && time < 11.27) {
             highlight('#3');    
+            captionTextArea.innerHTML = document.getElementById("3").innerText; 
     } else if (time >= 11.27 && time < 13.96) {
             highlight('#4');    
+            captionTextArea.innerHTML = document.getElementById("4").innerText; 
     }  else if (time >= 13.96 && time < 17.94) {
             highlight('#5');    
+            captionTextArea.innerHTML = document.getElementById("5").innerText; 
     } else if (time >= 17.94 && time < 22.37) {
             highlight('#6');    
+            captionTextArea.innerHTML = document.getElementById("6").innerText; 
     } else if (time >= 22.37 && time < 26.88) {
             highlight('#7');    
+            captionTextArea.innerHTML = document.getElementById("7").innerText; 
     } else if (time >= 26.88 && time < 30.92) {
             highlight('#8');    
+            captionTextArea.innerHTML = document.getElementById("8").innerText; 
     } else if (time >= 32.10 && time < 34.73) {
             highlight('#9');    
+            captionTextArea.innerHTML = document.getElementById("9").innerText; 
     } else if (time >= 34.73 && time < 39.43) {
             highlight('#10');    
+            captionTextArea.innerHTML = document.getElementById("10").innerText; 
     } else if (time >= 39.43 && time < 41.19) {
             highlight('#11');    
+            captionTextArea.innerHTML = document.getElementById("11").innerText; 
     } else if (time >= 42.35 && time < 46.30) {
             highlight('#12');    
+            captionTextArea.innerHTML = document.getElementById("12").innerText; 
     } else if (time >= 46.30 && time < 49.27) {
             highlight('#13');   
+            captionTextArea.innerHTML = document.getElementById("13").innerText; 
     } else if (time >= 49.27 && time < 53.76) {
             highlight('#14');    
+            captionTextArea.innerHTML = document.getElementById("14").innerText; 
     } else if (time >= 53.76 && time < 57.78) {
             highlight('#15');    
+            captionTextArea.innerHTML = document.getElementById("15").innerText; 
     } else if (time >= 57.78 && time < 59.00) {
             highlight('#16');    
+            captionTextArea.innerHTML = document.getElementById("16").innerText; 
     } else {
             highlight(null);
             if (video.currentTime === video.duration) {
@@ -129,11 +169,24 @@ function time(){
     }
 
 }
+function captionsclick(){
+  if(captions.classList.contains('no_display')){
+        captions.classList.add('display');
+        captions.classList.remove('no_display');
+    } else {
+        captions.classList.remove('display');
+        captions.classList.add('no_display');
+    } 
+}
+function changeSS(){
+}
 
+video.addEventListener('timeupdate', time, false);
+playpause.addEventListener('click', playPause, false);
+sound.addEventListener('click', soundOnOff, false);
+captionButton.addEventListener('click', captionsclick, false);
 
+$(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange', changeSS, false);
 
-video.ontimeupdate =time;
-playpause.onclick = playPause;
-sound.onclick = soundOnOff;
 
 });
